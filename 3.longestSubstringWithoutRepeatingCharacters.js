c348301620f91c0e4c3f391562de2e9c4e157b05
aleem-ahmed <w3st.io2021@gmail.com>
2022-05-19 13:50:22 -0400

    Improved

--- 3.longestSubstringWithoutRepeatingCharacters.js
+++ 3.longestSubstringWithoutRepeatingCharacters.js
@@ -2,42 +2,38 @@
 * @param {string} s
 * @return {number}
 */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function(str) {
 	let cache = {}
-	let i = 0
 	let ii = 0
 	let maxlength = 0
 
-	while (i < s.length) {
-		// If the value does not exist in the cache set it to 1
-		if (cache[s[i]] === undefined) { cache[s[i]] = true }
-		else if (cache[s[i]]) {
-			// While the cache of this is defined
-			while (cache[s[i]] != undefined) {
-				// if the cache record is 1
-				if (cache[s[ii]]) {
-					delete cache[s[ii]]
-				}
-				else {
-					cache[s[ii]]--
+	// For each character in the string
+	for (let i = 0; i < str.length; i++) {
+		const char = str[i]
+
+		// If the character does not exist in cache set it to true
+		if (!cache[char]) { cache[char] = true }
+		else {
+			while (cache[char]) {
+				const char2 = str[ii]
+
+				// if the cache record exists
+				if (cache[char2]) {
+					delete cache[char2]
 				}
 				
 				// [INCREMENT]
 				ii++
 			}
 			
-			cache[s[i]] = true
+			cache[char] = true			
 		}
-
-		maxlength = Math.max(maxlength , i - ii + 1)
-
-		// [INCREMENT]
-		i++
 		
-		console.log(cache);
+		maxlength = Math.max(maxlength, i - ii + 1)
+		//console.log(cache);
 	}
-
+	
 	return maxlength
 }
 
-console.log(lengthOfLongestSubstring('abcabcbb'));
+//console.log(lengthOfLongestSubstring('abcabcbb'))
