2b798d383eda3f66207dcfd0322679957d7bfb74
aleem-ahmed <w3st.io2021@gmail.com>
2022-05-22 23:53:49 -0400

    30 Combination of sums

---
+++ 39.combinationSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+	for (let can of candidates) {
+		console.log(can);
+	}
+}
+
+combinationSum([2,3,6,7], 7)
+combinationSum([2,3,5], 8)
+combinationSum([2], 1)
