13b4d9720fdea70cdeaa7d4262c1987279174d7f
aleem-ahmed <w3st.io2021@gmail.com>
2022-05-22 23:02:09 -0400

    Improved

--- 8.stringToInteger.js
+++ 8.stringToInteger.js
@@ -1,35 +1,36 @@
-var myAtoi = function(input) {
+var myAtoi = function(string) {
+    const INT_MIN = -Math.pow(2, 31);
+    const INT_MAX = Math.pow(2,31) - 1;
+
     let sign = 1; 
     let result = 0;
     let i = 0;
-    let n = input.length;
-    
-    let INT_MAX = Math.pow(2,31) - 1;
-    let INT_MIN = -Math.pow(2, 31);
         
-    // Discard all spaces from the beginning of the input string.
-    while (i < n && input[i] == ' ') { 
-        i++; 
+    // Skip all spaces
+    while (i < string.length && string[i] == ' ') { 
+        i++;
     }
 
     // sign = +1, if it's positive number, otherwise sign = -1. 
-    if (i < n && input[i] == '+') {
+    if (i < string.length && string[i] == '+') {
         sign = 1;
         i++;
     }
-	else if (i < n && input[i] == '-') {
+	else if (i < string.length && string[i] == '-') {
         sign = -1;
         i++;
     }
 
-    // Traverse next digits of input and stop if it is not a digit. 
+    // Traverse next digits of string and stop if it is not a digit. 
     // End of string is also non-digit character.
-    while (i < n && input[i] >= '0' && input[i] <= '9') {
-        let digit = input[i] - '0';
+    while (i < string.length && string[i] >= '0' && string[i] <= '9') {
+        let digit = string[i] - '0';
 
         // Check overflow and underflow conditions. 
-        if ((result > Math.floor(INT_MAX / 10)) || 
-            (result == Math.floor(INT_MAX / 10) && digit > INT_MAX % 10)) {     
+        if (
+			(result > Math.floor(INT_MAX / 10)) ||
+			(result == Math.floor(INT_MAX / 10) && digit > INT_MAX % 10)
+		) {     
             // If integer overflowed return 2^31-1, otherwise if underflowed return -2^31.    
             return sign == 1 ? INT_MAX : INT_MIN;
         }
