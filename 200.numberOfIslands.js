34af91abac86b6274a95fc9e3bbfd416832b2c89
aleem ahmed <w3st.io2021@gmail.com>
2022-08-25 15:23:06 -0400

    Added notes

--- 200.numberOfIslands.js
+++ 200.numberOfIslands.js
@@ -1,7 +1,11 @@
-/*
- * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+/**
+ * @notice
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's
+ * (water), return the number of islands.
  * 
- * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ * An island is surrounded by water and is formed by connecting adjacent lands
+ * horizontally or vertically. You may assume all four edges of the grid are all
+ * surrounded by water.
  * 
  * Example 1:
  * 
@@ -32,6 +36,7 @@
 */
 
 /**
+ * @notice Return answer
  * @param {character[][]} grid
  * @return {number}
 */
